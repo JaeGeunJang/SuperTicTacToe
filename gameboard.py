a7a9ce6f5d7b2eedb5ece083ef3e366f03b39de8
JaeGeunJang <jaekeun950@gmail.com>
2020-05-15 23:06:10 +0900

    gameboard change

--- gameboard.py
+++ gameboard.py
@@ -1,38 +1,71 @@
-import pygame
+import pygame as pg
+pg.init()
+width, height = 900,900
 
 WHITE = (255,255,255)
-RED = (255,0,0)
+BLACK = (0,0,0)
 BLUE = (0,0,255)
+RED = (255,0,0)
+BACKGROUND = (20, 190, 170)
+LINES = (15, 160, 145)
+BLINE = (10, 100, 100)
+pos = [[0,0,width/3,height/3],[width/3,0,width/3,height/3],[width*2/3,0,width/3,height/3],
+       [0,height/3,width/3,height/3],[width/3,height/3,width/3,height/3],
+       [width*2/3,height/3,width/3,height/3],[0,height*2/3,width/3,height/3],
+       [width/3,height*2/3,width/3,height/3],[width*2/3,height*2/3,width/3,height/3]]
+minipos = [[0,0],[width/9,0],[width*2/9,0],
+           [0,height/9], [width/9,height/9],[width*2/9,height/9],
+           [0,height*2/9], [width/9, height*2/9],[width*2/9,height*2/9]]
 
-pad_width = 900
-pad_height = 900
 
-def runGame() :
-    global gamepad, clock
+linewidth = int(height/100)
+halfwidth = int(height/200)
 
-    crashed = False
-    while not crashed :
-        for event in pygame.event.get() :
-            if event.type == pygame.QUIT :
-                crashed = True
-        gamepad.fill(BLUE)
-        pygame.display.update()
-        gamepad.fill(RED)
-        pygame.display.update()
-        gamepad.fill(WHITE)
-        pygame.display.update()
-        if event.type == pygame.KEYUP :
-            crashed = True
-        clock.tick(30)
-    pygame.quit()
+markwidth = int(width/24)
 
-def initGame() :
-    global gamepad, clock
-    pygame.init()
-    gamepad = pygame.display.set_mode((pad_width, pad_height))
-    pygame.display.set_caption('Super TicTacToe')
+nposi = 5
 
-    clock = pygame.time.Clock()
-    runGame()
+screen = pg.display.set_mode((width, height))
+done = True
+pg.display.set_caption("Python Super TicTacToe")
+clock = pg.time.Clock()
 
-initGame()
+def draw_background(screen, num) :
+    for i in range (1,9) :
+        if i%3 != 0  :
+            pg.draw.line(screen, LINES, [width*i/9,0],[width*i/9, height],halfwidth)
+            pg.draw.line(screen, LINES, [0,height*i/9],[width, height*i/9],halfwidth)
+    for i in range (1,3) :
+        pg.draw.line(screen, BLINE, [width*i/3,0],[width*i/3, height],linewidth)
+        pg.draw.line(screen, BLINE, [0,height*i/3],[width, height*i/3],linewidth)
+    pg.draw.rect(screen, RED,pos[num-1],linewidth)
+
+def drawX(screen,x,y) :
+    pg.draw.line(screen, RED, [x-markwidth,y-markwidth],[x+markwidth,y+markwidth],linewidth*2)
+    pg.draw.line(screen, RED, [x+markwidth,y-markwidth],[x-markwidth,y+markwidth],linewidth*2)
+
+
+def drawO(screen, x, y) :
+    x = int(x)
+    y = int(y)
+    pg.draw.circle(screen, BLUE, [x,y],markwidth , linewidth)
+
+while done :
+    screen.fill(BACKGROUND)
+    clock.tick(10)
+    posi = pg.mouse.get_pos()
+    for i in range(9) :
+        if pos[i][0] < posi[0] < pos[i][0]+pos[i][2] and pos[i][1] < posi[1] < pos[i][1]+pos[i][3] :
+            nposi = i+1
+
+
+    for event in pg.event.get() :
+        if event.type == pg.QUIT :
+            done = False
+        if event.type== pg.KEYUP :
+            done = False
+    draw_background(screen, nposi)
+
+    pg.display.flip()
+
+pg.quit()
